0a3ba653c12968bad5468f05f903bd26ede5bd86
Ahmed Elgohary <ahmed.elgohary.webdev@gmail.com>
2024-06-11 08:28:18 +0300

    Edit seeder

--- database/seeders/VariantAttributeValueSeeder.php
+++ database/seeders/VariantAttributeValueSeeder.php
@@ -10,21 +10,5 @@ class VariantAttributeValueSeeder extends Seeder
     public function run(): void
     {
         VariantAttributeValue::factory(40)->create();
-
-
-//        $variants = Variant::where('product_id', 1)->get();
-//        $variants->each(function (Variant $variant) {
-//            VariantAttributeValue::factory()->create([
-//                'variant_id' => $variant->id,
-//            ]);
-//        });
-//
-//
-//        AttributeValue::all()->each(function (AttributeValue $attributeValue) {
-//            VariantAttributeValue::factory()->create([
-//                'attribute_value_id' => $attributeValue->id,
-//            ]);
-//        });
-
     }
 }
